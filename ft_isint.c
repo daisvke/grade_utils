3dbb0f1ca116054d301a3397bd9bc63e7194d160
dtanigaw <daisuketanigawa@live.fr>
2021-03-14 02:16:48 +0100

    Makefile2

--- ft_isint.c
+++
@@ -1,9 +0,0 @@
-#include "header.h"
-
-int	ft_isint(char *s)
-{
-	while (*s)
-		if (!ft_isdigit(*s++))
-			return (0);
-	return (1);
-}
--- ft_isws.c
+++
@@ -1,4 +0,0 @@
-int	ft_isws(int c)
-{
-	return (c == 32 || (c >= 9 && c <= 13));
-}
